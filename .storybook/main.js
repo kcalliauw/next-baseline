e50e59097466e3c459b3d2d7728b478a2d7bc4f0
Koen Calliauw <koen@layer7.be>
2022-09-16 14:16:03 +0200

    feat: whole new version

---
+++ .storybook/main.js
@@ -0,0 +1,50 @@
+const { default: next } = require('next');
+const path = require('path');
+
+module.exports = {
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  /** Expose public folder to storybook as static */
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-css-modules-preset',
+    'storybook-addon-next-router',
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (baseConfig) => {
+    return {
+      ...baseConfig,
+      resolve: {
+        ...baseConfig.resolve,
+        modules: [path.resolve('./pages'), ...baseConfig.resolve.modules],
+        fallback: {
+          timers: false,
+          tty: false,
+          os: false,
+          http: false,
+          https: false,
+          zlib: false,
+          util: false,
+          ...baseConfig.resolve.fallback,
+        },
+      },
+    };
+  },
+};
