1e48e35087f4129d6202f66f89f29b85a5b823d9
Koen Calliauw <koen@layer7.be>
2022-09-17 09:43:37 +0200

    fix: rendering issues with multiple link children

--- components/Header.jsx
+++ components/Header.jsx
@@ -149,12 +149,10 @@ function NavItem({ href, children }) {
             : 'hover:text-teal-500 dark:hover:text-teal-400'
         )}
       >
-        <div>
-          {children}
-          {isActive && (
-            <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
-          )}
-        </div>
+        {children}
+        {isActive && (
+          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+        )}
       </Link>
     </li>
   );
